e413b9a7d128528ab5aa0d8b6ae617e8c9f9ccb7
trevorhartman <trevorhartman@trevorhartman.local>
2009-12-13 15:48:09 -0800

    fixed cardinality bug: missing belongs_to :user

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,10 +1,15 @@
-# require 'attr_encrypted'
-
 class Server < ActiveRecord::Base
   attr_encrypted :password, :key => '393b79433f616f445f652a752d', :attribute => 'crypted_password'
   
+  belongs_to :user
+  
   has_many :site_servers
   has_many :sites, :through => :site_servers
+  
+  validates_presence_of :url, :on => :create, :message => "URL is required."
+  validates_presence_of :username, :on => :create, :message => "Username is required."
+  validates_presence_of :password, :on => :create, :message => "Password is required."
+  
   
   def name
     username + "@" + url
