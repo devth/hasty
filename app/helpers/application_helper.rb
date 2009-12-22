dc23aa9f9e4f13b2bb657c6dbb7d7dff86f0098b
trevorhartman <trevorhartman@gmail.com>
2009-12-21 19:48:54 -0800

    form helper to focus first input

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  # form helper
+  def set_form_focus
+    javascript_tag("$(document).ready( function() { $('form:first input:first').focus(); });")
+  end
+  
 end
